b63c6bb67d5b04641ffff59e8cffd261521f89d7
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-08-02 20:08:31 +0600

    task 4 done

--- string.js
+++ string.js
@@ -1,28 +1,7 @@
-/* ### Task-1: 
+/*
+### Task-1: 
 Count how many times a string has the letter `a`
-
----
-### Task-2: 
-Count how many times a string has the letter `a` or `A`
-
----
-
-### Task-3: 
-
-Check whether a string contains all the vowels `a`, `e`, `i`, `o`, `u` 
-
----
-
-### Task-4: 
-If a given string has either x, replace x by y. if the given string has X, replace it by Y.
-
-    Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
-
----
-### Task-5:
-Capitalize Every first Letter of each word in a String */
-
-// 1
+*/
 
 let str = "i am a savage";
 let strLower = str.toLowerCase();
@@ -34,7 +13,10 @@ for (let i = 0; i <= str.length; i++) {
 }
 console.log(count);
 
-// 2
+/*
+### Task-2: 
+Count how many times a string has the letter `a` or `A`
+*/
 
 let str2 = "i am a sAvAge and also a nerd";
 let count1 = 0;
@@ -51,7 +33,11 @@ for (let i = 0; i <= str2.length; i++) {
 console.log("a = " + count1);
 console.log("A = " + count2);
 
-// 3
+/*
+### Task-3: 
+
+Check whether a string contains all the vowels `a`, `e`, `i`, `o`, `u` 
+*/
 
 let prb = "savage";
 prb = prb.toLowerCase();
@@ -66,3 +52,21 @@ if (
 } else {
   console.log("All vowel are not in present");
 }
+
+/*
+### Task-4: 
+If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+    Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
+*/
+
+let text = "xsavageX";
+console.log(text);
+let text1 = text.replace('x', 'y').replace('X','Y');
+
+console.log(text1);
+
+/*
+### Task-5:
+Capitalize Every first Letter of each word in a String 
+*/
